ae58626a83115e5369860aa92c223445b8423646
Marina frank <mfrankca@yahoo.ca>
2024-06-20 21:36:58 -0400

    2

--- app_streamlit.py
+++ app_streamlit.py
@@ -445,8 +445,9 @@ def main():
         if file1 and file2:
             new_entries, deleted_entries = compare_catalogs(file1, file2, file_type)
 
-            output_file = "comparison_result.xlsx"
-            save_comparison_result(new_entries, deleted_entries, output_file)
+            add_output_file = "new_entries_result.csv"
+            delete_output_file = "delete_entries_result.csv"
+            save_comparison_result(new_entries, deleted_entries, add_output_file,delete_output_file)
 
             st.success(f"Comparison complete! Download the result below.")
             st.download_button("Download comparison result", data=open(output_file, "rb").read(), file_name=output_file)
