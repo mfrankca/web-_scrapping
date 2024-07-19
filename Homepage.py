822ea7fae0d3082bf2d8aaf93e2da2714a07f26a
Marina frank <mfrankca@yahoo.ca>
2024-07-19 01:27:21 -0400

    2

--- Homepage.py
+++ Homepage.py
@@ -4,5 +4,5 @@ image_path = "uploads//logo.png"
 st.mage(image_path, use_column_width=True)
 st.title("Welcome to SunRayCity Managment")  
 
-st.markdown("# Welcome to SunRayCity 🎈")
-st.sidebar.markdown("# Homepage 🌼")
+#st.markdown("# Welcome to SunRayCity 🎈")
+#st.sidebar.markdown("# Homepage 🌼")
