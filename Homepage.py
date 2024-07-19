7cb8b5934916256524fb892305aad80c18ef7d14
Marina frank <mfrankca@yahoo.ca>
2024-07-19 01:29:36 -0400

    6

--- Homepage.py
+++ Homepage.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 image_path = "uploads//logo.png"
-st.mage(image_path, use_column_width=True)
+st.image(image_path, use_column_width=True)
 st.title("Welcome to SunRayCity Managment")  
 
-#st.markdown("# Welcome to SunRayCity 🎈")
-#st.sidebar.markdown("# Homepage 🌼")
+st.markdown("# Welcome to SunRayCity 🎈")
+st.sidebar.markdown("# Homepage 🌼")
