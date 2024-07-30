3ba5ddc6f4a53b82c5e156e42b852b982ecc9eb1
Marina frank <mfrankca@yahoo.ca>
2024-07-30 19:26:47 -0400

    1

--- pages/example_selenium.py
+++ pages/example_selenium.py
@@ -1,37 +1,30 @@
 import streamlit as st
 
-"""
-## Web scraping on Streamlit Cloud with Selenium
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
-
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
-
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+import re
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import chromedriver_autoinstaller
    
+ # Automatically download and install the correct version of chromedriver
+chromedriver_autoinstaller.install()
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+# Set up headless Chrome
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+# Initialize the WebDriver
+driver = webdriver.Chrome(options=chrome_options)
 
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://www.ebay.com/fdbk/feedback_profile/sunraycity")
+driver.get("https://www.ebay.com/fdbk/feedback_profile/sunraycity")
+driver.quit()
 
-    st.code(driver.page_source)
