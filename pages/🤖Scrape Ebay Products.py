33949b4733b9e313b634440ac19551762923f9b1
Marina frank <mfrankca@yahoo.ca>
2024-07-30 23:07:54 -0400

    2

--- pages/🤖Scrape Ebay Products.py
+++ pages/🤖Scrape Ebay Products.py
@@ -209,6 +209,7 @@ def generate_output_files(data, output_format):
         output_files.append(csv_file) 
 
     return output_files
+
 image_path = "uploads//logo.png"
 st.sidebar.image(image_path, use_column_width=True)
 st.title("Welcome to SunRayCity Managment")  
