d53c5298f88b5e0de8a355b7fcdde7af8e447588
Marina frank <mfrankca@yahoo.ca>
2024-08-04 19:44:08 -0400

    1

--- pages/product search.py
+++ pages/product search.py
@@ -30,7 +30,7 @@ def parse(soup):
 def output(productslist, searchterm):
     productsdf =  pd.DataFrame(productslist)
     productsdf.to_csv(searchterm + 'output.csv', index=False)
-    print('Saved to CSV')
+    st.write('Saved to CSV')
     return
 
 soup = get_data(searchterm)
