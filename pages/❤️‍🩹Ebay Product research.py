b8c178e38c1c3995fecde14c5db3aeda59f3b344
Marina frank <mfrankca@yahoo.ca>
2024-08-04 22:16:31 -0400

    2

--- pages/❤️‍🩹Ebay Product research.py
+++ pages/❤️‍🩹Ebay Product research.py
@@ -7,7 +7,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
