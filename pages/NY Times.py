6fdd8cce817fdfee462ddf1a1d588dc1218d42f2
Marina frank <mfrankca@yahoo.ca>
2024-08-09 01:26:59 -0400

    2

--- pages/NY Times.py
+++ pages/NY Times.py
@@ -11,7 +11,7 @@ try:
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="114.0.5735.90").install()),
+    webdriver.Chrome(service=Service(ChromeDriverManager(version="114.0.5735.90").install()),
                                     options=options)
     # Get the New York Times page
     driver.get("https://nytimes.com")
