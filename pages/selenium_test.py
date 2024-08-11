7d0ca98ae6b5afd234cc853d1309b74f25c4a17f
Marina frank <mfrankca@yahoo.ca>
2024-08-11 19:41:15 -0400

    4

--- pages/selenium_test.py
+++ pages/selenium_test.py
@@ -10,32 +10,52 @@ username = "reddiveusa@gmail.com"
 password = "Profit44" 
 
 # Initialize Selenium WebDriver
-####driver = webdriver.Chrome()  # Ensure you have the Chrome WebDriver installed
-'''
-# Step 1: Login to eBay
-driver.get('https://www.ebay.com/sh/research?marketplace=EBAY-US&tabName=SOLD')
-time.sleep(2)  # Wait for the login page to load
+
+def get_website_content(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+        time.sleep(5)
+        html_doc = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup.get_text()
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
+# ---------------- Page & UI/UX Components ------------------------
+def main_sidebar():
+    # 1.Vertical Menu
+    st.header("Running Selenium on Streamlit Cloud")
+    site_extraction_page()
 
 
-# Find the username field and enter the username
-username_field = driver.find_element(By.ID, 'userid')
-username_field.send_keys(username)
-username_field.send_keys(Keys.RETURN)
-time.sleep(2)  # Wait for the password field to load
+def site_extraction_page():
+    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
+    url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
 
-# Find the password field and enter the password
-password_field = driver.find_element(By.ID, 'pass')
-password_field.send_keys(password)
-password_field.send_keys(Keys.RETURN)
-time.sleep(5)  # Wait for the login process to complete
+    clicked = st.button("Load Page Content",type="primary")
+    if clicked:
+        with st.container(border=True):
+            with st.spinner("Loading page website..."):
+                content = get_website_content(url)
+                st.write(content)
 
-# Step 2: Extract cookies from Selenium and add them to the requests session
-session = requests.Session()
-for cookie in driver.get_cookies():
-    session.cookies.set(cookie['name'], cookie['value'])
-'''
-# Step 3: Use the session to scrape the desired page
 
+if __name__ == "__main__":
+    main_sidebar()
+    
+"""   
 url = 'https://www.ebay.com/itm/294453072910'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -43,3 +63,4 @@ soup = BeautifulSoup(response.content, 'html.parser')
 title_element = soup.find('h1', attrs={'class': 'x-item-title__mainTitle'})
 title=title_element.text.replace('Details about', '').strip() if title_element else 'Not Available'
 st.write(title)
+"""
