04fc36a1adfb99984e6cc3d031c313a48e49f3b3
Marina frank <mfrankca@yahoo.ca>
2024-08-11 22:17:40 -0400

    5

--- pages/❤️‍🩹Ebay Reviews Test.py
+++ pages/❤️‍🩹Ebay Reviews Test.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import re
 
 # Configuration from Streamlit secrets
