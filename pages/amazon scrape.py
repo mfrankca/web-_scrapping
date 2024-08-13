a706d7c29fba5d6ba80d518d60963055643b1fda
Marina frank <mfrankca@yahoo.ca>
2024-08-13 00:02:12 -0400

    7

--- pages/amazon scrape.py
+++ pages/amazon scrape.py
@@ -90,8 +90,9 @@ def main(URL):
 
 if __name__ == '__main__':
   # opening our url file to access URLs
-    file = open("url.txt", "r")
+    ###file = open("url.txt", "r")
 
     # iterating over the urls
-    for links in file.readlines():
-        main(links)
+    ###for links in file.readlines():
+    links=    url='https://www.amazon.ca/Stylish-Non-prescription-Eyeglasses-Eyeglass-Gradient/dp/B07PY8MQ5M/ref=sr_1_1?crid=3U06ACJEVNQL8&dib=eyJ2IjoiMSJ9.0DPMaxiy5RHMOAe3jYgiNjx2U_l2vejKryfnIhJP_Oe42X7-Ap5be7C2T_71pY-smlLrwMiMkaYU4Ar_4U-1pFMpnwohhSYc44lJCOEjtkPk4zs0eruAY0F65uuVuX_wB6wXFuJwj1Uu0lgjbnmsbxqDCsV94Mpsak6y6j6FL9qPTZ-jO9nlkaqOvZNnvxI16nZcrWAwVyGGq0Vv4jG2sxLA8J6sH2dEAxOPJrky_qxKMjz-wsNpJUo1oN-7ifGgdPD9SeHwfrcwXQA6XpUf1ZsTv5BF1l3rbkzzJr8Ict4.lmcMBgfroCnMyvnQA1bMm6vmzd3t_PEzmPYOxZFUNOk&dib_tag=se&keywords=Fendi+Eyeglasses&qid=1723517219&sprefix=fendi+eyeglasses%2Caps%2C64&sr=8-1'  # Replace with the product URL
+    main(links)
