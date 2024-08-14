e5500c52e2d8657cb6100ef749d747f8c2a9cf62
Marina frank <mfrankca@yahoo.ca>
2024-08-13 22:53:05 -0400

    4

--- pages/🐰Web Scrape Products.py
+++ pages/🐰Web Scrape Products.py
@@ -42,7 +42,7 @@ def display_sidebar():
         Supported feedback sites: **eBay Feedback Site 1**, **eBay Feedback Site 2**.
         """)
 # Connect to Website and pull in data
-URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+#URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
@@ -56,9 +56,9 @@ headers = {
 # Function to handle scraping from different e-commerce stores
 def scrape_product_data(store, query, country):
     if store == 'eBay':
-        return EbayScraper.Items(query, country=country)
+        return EbayScraper.Items()
     elif store == 'Amazon':
-        return AmazonScraper.AmazonItems(query, country=country)
+        return AmazonScraper.perform_web_scraping()
     elif store == 'Walmart':
         # Implement scraping logic for Walmart here
         pass
