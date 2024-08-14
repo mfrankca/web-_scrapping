9581fb86c0b3d75e6cc8d7da1b7b357fe90d4815
Marina frank <mfrankca@yahoo.ca>
2024-08-13 22:48:02 -0400

    3

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -1,38 +1,134 @@
-from bs4 import BeautifulSoup
+
 import requests
+import EbayScraper
+import AmazonScraper
+import streamlit as st
+
 import streamlit as st
 import pandas as pd
 from io import BytesIO
+import urllib.parse
+import urllib.request
+from bs4 import BeautifulSoup
 
-# Dictionary for countries (no specific conditions or types for Amazon)
-countryDict = {
-    'au': '.com.au',
-    'ca': '.ca',
-    'de': '.de',
-    'es': '.es',
-    'fr': '.fr',
-    'in': '.in',
-    'it': '.it',
-    'jp': '.jp',
-    'uk': '.co.uk',
-    'us': '.com',
-}
 
-def AmazonItems(query, country='us'):
+def scrape_amazon(item):
+    url = f'https://www.amazon.com//itm/{item}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
     
-    if country not in countryDict:
-        raise Exception('Country not supported, please use one of the following: ' + ', '.join(countryDict.keys()))
+    row = {'Listing ID': item}
+    page = requests.get(URL)
+
+    # Check and print the title
+    title_element = soup1.find(id='productTitle')
+    if title_element:
+        title = title_element.get_text(strip=True)
+        st.write("Title:", title)
+    else:
+        st.write("Title not found")
+
+    # Check and print the rating
+    rating_element = soup1.find(id='acrCustomerReviewText')
+    if rating_element:
+        rating = rating_element.get_text(strip=True)
+        st.write("Rating:", rating)
+    else:
+        st.write("Rating not found")
+        
+
+    def perform_web_scraping(input_filepath):
+        # Determine the file type and read the data accordingly
+        _, file_extension = os.path.splitext(input_filepath)
+        
+        if file_extension == '.csv':
+            listings = pd.read_csv(input_filepath)
+        elif file_extension == '.txt':
+            listings = pd.read_csv(input_filepath, header=None, names=['item'])
+        else:
+            st.error("Unsupported file type. Please upload a CSV or TXT file.")
+            return []
+
+        data = []
+        for item in listings['item']:
+            item_data = scrape_amazon(item)
+            data.append(item_data)
+
+        return data
     
-    soup = __GetAmazonHTML(query, country)
-    data = __ParseAmazonItems(soup)
+def generate_output_files(data, output_format):
+    output_files = []
+    df = pd.DataFrame(data)   
+    # Convert the data to a DataFrame
+    #df = pd.DataFrame([data], columns=columns_order)
+    filtered_df = df[columns_order]
     
-    return data
+    if 'Excel' in output_format :
+        excel_file = 'output.xlsx'
+        filtered_df.to_excel(excel_file, index=False)
+        output_files.append(excel_file)
+    if 'JSON' in output_format :
+        json_file = 'output.json'
+        filtered_df.to_json(json_file, orient='records')
+        output_files.append(json_file)
+    if 'CSV' in output_format:
+        csv_file = 'output.csv'
+        filtered_df.to_csv(csv_file, index=False)
+        output_files.append(csv_file) 
+
+    return output_files
+
+#image_path = "uploads//logo.png"
+#st.sidebar.image(image_path, use_column_width=True)
+#st.title("Welcome to SunRayCity Management")  
+
+st.write('Upload a file with listing numbers and select the output file format.')
+uploaded_file = st.file_uploader('Choose a file', type=['csv', 'txt'])
+output_format = st.multiselect('Select output format', ['Excel', 'JSON', 'CSV'])
+
+if uploaded_file is not None:
+    if st.button('Scrape Data'):
+        # Save uploaded file temporarily
+        input_filepath = os.path.join('temp', uploaded_file.name)
+        with open(input_filepath, 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+
+        # Perform web scraping
+        data = perform_web_scraping(input_filepath)
+
+        if data:
+            # Generate output files
+            output_files = generate_output_files(data, output_format)
+
+            st.success('Scraping and file generation completed successfully!')
+
+            for file in output_files:
+                with open(file, 'rb') as f:
+                    btn = st.download_button(
+                        label=f"Download {file}",
+                        data=f,
+                        file_name=file,
+                        mime='application/octet-stream'
+                    )
+
+if not os.path.exists('temp'):
+    os.makedirs('temp')
+
+    
+    
+    
+    
+    
+    
+    
+    
 
 def __GetAmazonHTML(query, country):
     # Build the URL
     parsedQuery = urllib.parse.quote(query).replace('%20', '+')
     #url = f'https://www.amazon{countryDict[country]}/s?k=' + parsedQuery
-    url='https://www.amazon.ca/Stylish-Non-prescription-Eyeglasses-Eyeglass-Gradient/dp/B07PY8MQ5M/ref=sr_1_1?crid=3U06ACJEVNQL8&dib=eyJ2IjoiMSJ9.0DPMaxiy5RHMOAe3jYgiNjx2U_l2vejKryfnIhJP_Oe42X7-Ap5be7C2T_71pY-smlLrwMiMkaYU4Ar_4U-1pFMpnwohhSYc44lJCOEjtkPk4zs0eruAY0F65uuVuX_wB6wXFuJwj1Uu0lgjbnmsbxqDCsV94Mpsak6y6j6FL9qPTZ-jO9nlkaqOvZNnvxI16nZcrWAwVyGGq0Vv4jG2sxLA8J6sH2dEAxOPJrky_qxKMjz-wsNpJUo1oN-7ifGgdPD9SeHwfrcwXQA6XpUf1ZsTv5BF1l3rbkzzJr8Ict4.lmcMBgfroCnMyvnQA1bMm6vmzd3t_PEzmPYOxZFUNOk&dib_tag=se&keywords=Fendi+Eyeglasses&qid=1723517219&sprefix=fendi+eyeglasses%2Caps%2C64&sr=8-1'  # Replace with the product URL
+   # Connect to Website and pull in data
+    URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
     st.write(url)
     # Get the web page HTML
     # Making the HTTP Request
---
+++ pages/🐰Web Scrape Products.py
@@ -0,0 +1,124 @@
+
+import requests
+import EbayScraper
+import AmazonScraper
+import streamlit as st
+
+import streamlit as st
+import pandas as pd
+from io import BytesIO
+import urllib.parse
+import urllib.request
+from bs4 import BeautifulSoup
+
+def display_sidebar():
+    """
+    Display the sidebar with a logo image and documentation.
+    """
+    image_path = "uploads/logo.png"
+    st.sidebar.image(image_path, use_column_width=True)
+
+    with st.sidebar.expander("Documentation", icon="📚"):
+       st.write("""
+        **Welcome to the SunRayCity Management Tool**
+
+        ### About SunRayCity Sunglasses
+        At SunRayCity Sunglasses, we search the world for the best deals on fashion and sport sunglasses. We only sell authentic and brand name sunglasses. If you are searching for a specific model that you cannot find on the site, send us an email at [sales@sunraycity.com](mailto:sales@sunraycity.com) and we will do our best to find it. We operate online only to offer these deals.
+
+        ### Features
+        - **Manage Customers**: Keep track of customer details and interactions.
+        - **Product Catalog Management**: Maintain and update the product catalog.
+        - **eBay Product Catalog Scraping**: Scrape product data from eBay.
+        - **Scrape eBay Reviews**: Scrape reviews from the following eBay feedback pages:
+          - [SunRayCity eBay Store 1](https://www.ebay.com/fdbk/feedback_profile/sunraycity)
+          - [SunRayCity eBay Store 2](https://www.ebay.com/fdbk/feedback_profile/sunraycity_store)
+        - **Compare Product Catalogs**: Compare the product catalog on eBay vs. the SunRayCity website.
+
+        ### Instructions
+        1. **Choose Site**: Select which eBay feedback site to scrape.
+        2. **Enter URL**: Provide the URL of the eBay feedback page.
+        3. **Scrape Data**: Click "Scrape Data" to collect and save reviews.
+
+        Supported feedback sites: **eBay Feedback Site 1**, **eBay Feedback Site 2**.
+        """)
+# Connect to Website and pull in data
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "DNT": "1",
+    "Connection": "close",
+    "Upgrade-Insecure-Requests": "1"
+}
+
+# Function to handle scraping from different e-commerce stores
+def scrape_product_data(store, query, country):
+    if store == 'eBay':
+        return EbayScraper.Items(query, country=country)
+    elif store == 'Amazon':
+        return AmazonScraper.AmazonItems(query, country=country)
+    elif store == 'Walmart':
+        # Implement scraping logic for Walmart here
+        pass
+
+    return []
+
+# Streamlit app
+def main():
+    st.title('Product Data Scraper')
+
+    
+    display_sidebar()
+
+    # Dropdown to select the e-commerce store
+    store = st.selectbox("Select e-commerce Store", ["eBay", "Amazon", "Walmart"])
+    
+    # File uploader for product descriptions
+    uploaded_file = st.file_uploader("Upload a file with product descriptions", type=["csv", "xlsx"])
+
+    # Dropdown to select the country
+    #country = st.selectbox("Select Country", list(EbayScraper.countryDict.keys()))
+
+
+    if uploaded_file is not None:
+        # Read the uploaded file
+        if uploaded_file.name.endswith('.csv'):
+            product_df = pd.read_csv(uploaded_file)
+        else:
+            product_df = pd.read_excel(uploaded_file)
+        
+        st.write("Product Descriptions:")
+        st.dataframe(product_df)
+
+        # Button to start scraping
+        if st.button('Scrape Product Data'):
+            all_products_data = []
+
+            for product in product_df['Product']:
+                st.write(f"Scraping data for: {product}")
+                product_data = scrape_product_data(store, product, country)
+                all_products_data.extend(product_data)
+            
+            # Convert the list of dictionaries to a pandas DataFrame
+            result_df = pd.DataFrame(all_products_data)
+
+            # Save the DataFrame to an Excel file
+            excel_buffer = BytesIO()
+            result_df.to_excel(excel_buffer, index=False)
+            excel_buffer.seek(0)
+
+            # Download link for the Excel file
+            st.download_button(
+                label="Download Excel file",
+                data=excel_buffer,
+                file_name="product_data.xlsx",
+                mime="application/vnd.ms-excel"
+            )
+
+if __name__ == '__main__':
+    main()
+
+
+    
