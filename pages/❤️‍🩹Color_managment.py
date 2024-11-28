5fea1775193108d05662b7f488c7f306e75541ad
Marina frank <mfrankca@yahoo.ca>
2024-11-28 10:47:13 -0500

    5

--- pages/❤️‍🩹Color_managment.py
+++ pages/❤️‍🩹Color_managment.py
@@ -3,6 +3,11 @@ import pandas as pd
 from io import BytesIO
 import boto3
 
+# Fetch environment variables
+aws_access_key_id = st.secrets["aws_access_key_id"]
+aws_secret_access_key = st.secrets["aws_secret_access_key"]
+aws_default_region = st.secrets["aws_default_region"]
+
 def display_sidebar():
     """
     Display the sidebar with a logo image and documentation.
